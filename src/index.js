4d9bb522c0b5823c3c2ecc1a1d643105ffe5ed42
Aman Jhurani <aman.jhurani@hubilo.com>
2022-07-09 23:19:28 +0530

    update: hashrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
